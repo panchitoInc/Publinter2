68c6a57de7994d7a5a22e638ecb3ed0d4b9b0e11
hernan <hernanfaggiani@gmail.com>
2018-08-18 12:20:50 -0300

    antes de bajar cliente

--- AccesoDatos/Repository/MedioRepository.cs
+++ AccesoDatos/Repository/MedioRepository.cs
@@ -82,19 +82,40 @@ namespace AccesoDatos.Repository
 
                     medioBd.Descripcion = model.Descripcion;
                     medioBd.Nombre = model.Nombre;
-
                     if (model.Contactos != null && model.Contactos.Count > 0)
                     {
-                        medioBd.Contactos = model.Contactos;
+
+                        // quito los vacios 
+                        model.Contactos = model.Contactos.Where(x => x.ContactoId > 0 && x.Nombre != "").ToList();
+
+                        // contactos Nuevos
+                        var contactosNuevos = model.Contactos.Where(x => x.Delete.Equals(false) && x.ContactoId == 0 && x.Nombre != "" && (x.Telefono != "" || x.Email != "")).ToList();
+                        if (contactosNuevos != null)
+                        {
+                            SaveNewContacto(medioBd, contactosNuevos);
+                        }
+
+                        // los viejos que se pudireran modificar
+                        var contactosModificados = model.Contactos.Where(x => x.ContactoId > 0 && x.Delete.Equals(false)).ToList();
+                        if(contactosModificados != null)
+                        {
+                            UpdateContacto(medioBd, contactosModificados);
+                        }
+
+                        //medioBd.Contactos = model.Contactos.Where(x => x.Delete.Equals(false).ToList();
                         medioBd.Contactos.ForEach(x => context.Entry(x).State = System.Data.Entity.EntityState.Modified);
                     }
 
+                    context.SaveChanges();
+                    // Programas
                     if (model.Programas != null && model.Programas.Count() > 0)
                     {
-                        medioBd.Programas = model.Programas;
+                        //quiton los vacios.
+                        medioBd.Programas = model.Programas.Where(x => x.ProgramaId > 0 && x.Nombre !="").ToList();
+                        var ProgramasNuevos = model.Programas.Where(x =>x.Delete.Equals(false) && x.ProgramaId == 0 && x.Nombre != "" && x.HoraInicio  != null && x.PrecioSegundo > 0).ToList();
+
                         medioBd.Programas.ForEach(x => context.Entry(x).State = System.Data.Entity.EntityState.Modified);
                     }
-                    
                     
                     context.Entry(medioBd).State = System.Data.Entity.EntityState.Modified;
                     context.SaveChanges();
@@ -105,9 +126,37 @@ namespace AccesoDatos.Repository
             {
                 return false;
             }
-            
+        }
 
-                
+
+        public void UpdateContacto(Medio mediobd, List<Contacto>Nuevos)
+        {
+            foreach(var contAct in Nuevos)
+            {
+                var existingInBd = mediobd.Contactos
+                                    .Where(c => c.ContactoId == contAct.ContactoId)
+                                    .SingleOrDefault();
+
+                if(existingInBd != null)
+                {
+                    existingInBd.Ciudad = contAct.Ciudad;
+                    existingInBd.Departamento = contAct.Ciudad;
+                    existingInBd.Direccion = contAct.Direccion;
+                    existingInBd.Email = contAct.Email;
+                    existingInBd.Telefono = contAct.Telefono;
+                }
+
+            }
+
+        }
+
+        public void SaveNewContacto(Medio medioBd, List<Contacto> nuevos)
+        {
+            foreach(var c in nuevos)
+            {
+                medioBd.Contactos.Add(c);
+            }
+            
         }
     }
 }
