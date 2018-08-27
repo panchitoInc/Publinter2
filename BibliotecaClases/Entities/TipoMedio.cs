baefa2322998c387c768fe211779c6b285777318
hernan <hernanfaggiani@gmail.com>
2018-08-27 20:30:09 -0300

    a

---
+++ BibliotecaClases/Entities/TipoMedio.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModule.Entities
+{
+   public class TipoMedio
+    {
+        [Key, Column("TipoMedioId"), DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        
+    }
+}
---
+++ BibliotecaClases/EntitiesResult/Get_All_Cliente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataModule.EntitiesResult
+{
+    public class Get_All_Cliente
+    {
+        public int ClienteId { get; set; }
+        public string Nombre { get; set; }
+        public string RazonSocial { get; set; }
+        public string RUT { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+
+    }
+}
